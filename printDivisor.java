c0ee3585287685a0ff4e321d49542a14085a0a1f
trmpsanjay <trmpsanjay@gmail.com>
2022-07-02 20:45:50 +0530

    printDivisor added

---
+++ printDivisor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Solution {
+	
+	// Write your printDivisor function here
+    public void printDivisor(int n){
+        for(int i=1;i<=n;i++){
+            if(n%i==0) System.out.print(i+" ");
+        }
+    }
+	
+	public static void main(String args[]) {
+		Solution obj = new Solution();
+		Scanner sc =  new Scanner(System.in);
+		int n = sc.nextInt();
+		obj.printDivisor(n);
+	}
+}

c226675cc07dbda61d8068d65556a504e6803574
trmpsanjay <trmpsanjay@gmail.com>
2022-07-03 13:04:46 +0530

    sort012 added

---
+++ sort012.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+       int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(low<=high && mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
+            }else if(arr[mid]==1){
+                mid++;
+            }else{
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+//              mid++;
+                high--;
+            }
+        }
+    }
+}
